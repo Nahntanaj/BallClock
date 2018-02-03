94b99a434b860814f7c0d6081361978f2bab7684
Jonathan Stringer <jonathan.stringer@us.panasonic.com>
2018-02-02 17:20:39 -0700

    Added toString to go ballclock

--- BallClock-go/src/BallClock.go
+++ BallClock-go/src/BallClock.go
@@ -1,7 +1,10 @@
 package src
 
 import (
-
+  "fmt"
+  "buffer"
+  "os"
+  "strconv"
 )
 
 type BallClock struct { 
@@ -37,15 +40,79 @@ func incrementMinute(){
 	}
 }
 
-func main(reserveCapacity uint8) string{
-	if(reserveCapacity < 27){
-		//error message
-	} else if(reserveCapacity > 127){
-		//error message
-	} else {
-		ballclock = BallClock([5]uint8, [12]uint8, [12]uint8, [reserveCapacity]uint8)
-		 	
-	}
+func toString() string {
+  var buffer bytes.Buffer
+  buffer.writeString("{")
+  
+  //Mins
+  buffer.writeString("\"Min\":")
+  buffer.writeString("[")
+  if len(min) > 0 {
+    buffer.writeString(min[0])
+    for i:=1; i<len(min); i++ {
+      buffer.writeString(",")
+      buffer.writeString(min[i])  
+    }
+  }
+  buffer.writeString("]")
+  
+  //FiveMins
+  buffer.writeString(",\"FiveMin\":")
+  buffer.writeString("[")
+  if len(fiveMin) > 0 {
+    buffer.writeString(fiveMin[0])
+    for i:=1; i<len(fiveMin); i++ {
+      buffer.writeString(",")
+      buffer.writeString(fiveMin[i])  
+    }
+  }
+  buffer.writeString("]")
+  
+  //Hours
+  buffer.writeString(",\"Hour\":")
+  buffer.writeString("[")
+  if len(hour) > 0 {
+    buffer.writeString(hour[0])
+    for i:=1; i<len(hour); i++ {
+      buffer.writeString(",")
+      buffer.writeString(hour[i])  
+    }
+  }
+  buffer.writeString("]")
+  
+  buffer.writeString("}")
+  return buffer.String()
+}
+
+func main() {
+  args := os.Args
+  
+  if(len(args) == 2) {
+    reserveCapacity, err := Atoi(args[1])
+    
+    if(reserveCapacity < 27){
+      os.Exit(-1)
+    } else if(reserveCapacity > 127){
+      os.Exit(-1)
+    } else {
+      ballclock = BallClock([5]uint8, [12]uint8, [12]uint8, [reserveCapacity]uint8)
+    }
+  } else if (len(args) == 3 ) {
+    reserveCapacity, err := Atoi(args[1])
+    minutesToRun, err := Atoi(args[2])
+    
+    if err != nil {
+      os.Exit(-1)
+    }
+    
+    if(reserveCapacity < 27){
+      os.Exit(-1)
+    } else if(reserveCapacity > 127){
+      os.Exit(-1)
+    } else {
+      ballclock = BallClock([5]uint8, [12]uint8, [12]uint8, [reserveCapacity]uint8)
+    }
+  }
 		
 		
 	
